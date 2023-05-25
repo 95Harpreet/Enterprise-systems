b480d75ef2703fb52a8bb5eedb3edb515e8f33d9
95Harpreet <115647828+95Harpreet@users.noreply.github.com>
2023-05-25 13:46:58 -0400

    basic python programming

---
+++ basic python syntax.py
@@ -0,0 +1,26 @@
+#adding two variables
+a = 5
+b = 10
+c = a + b   
+print("sum= ",c)
+
+#subtracting two variables
+x=10
+y=5
+z=x-y
+print("sub= ",z)
+
+# Python program to illustrate while loop
+count = 0
+while (count < 3):
+    count = count + 1
+    print("Hello world")
+    
+# A simple Python function
+def fun():
+    print("Welcome to visual studio code")
+ 
+ 
+# Driver code to call a function
+fun()    
+    
